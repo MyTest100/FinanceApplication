84f8a3d24f32bcccde11d7e984553e813e3163e6
Gopinath <gopi@ev.com>
2022-06-15 11:56:12 +0530

    2 messages

--- First.cpp
+++ First.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	cout << "Welcome to GIT" << endl;
+int main() {	
 	cout << "Vanakkam GIT" << endl;	
 	cout << "Namaskara GIT" << endl; 
    return 0;
